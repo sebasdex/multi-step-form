3e70895bae5c9614883aa4f43556ed4e34e470dc
DexterDC <sebasdc9@hotmail.com>
2023-06-19 13:59:59 -0600

    first version

--- src/components/FormPersonal.jsx
+++ src/components/FormPersonal.jsx
@@ -5,7 +5,7 @@ function FormPersonal({ error, handleChange, formData }) {
                 <label htmlFor="name" className="md:flex justify-between">Name
                     {error.name && <span className="text-red-500 font-bold block">{error.name}</span>}
                 </label>
-                <input type="text" name="name" value={formData.name} onChange={handleChange} className='p-2 md:p-4 border block w-full' placeholder="e.g. Stephen King"
+                <input type="text" name="name" value={formData.name} onChange={handleChange} className='p-2 md:p-4 border block w-full rounded-md' placeholder="e.g. Stephen King"
                 />
             </div>
             <div className='space-y-2'>
@@ -13,13 +13,13 @@ function FormPersonal({ error, handleChange, formData }) {
                     {error.email && <span className="text-red-500 font-bold block">{error.email}</span>}
                 </label>
 
-                <input type="email" name="email" value={formData.email} onChange={handleChange} className='p-2 md:p-4 border block w-full' placeholder="e.g. stephenking@lorem.com" />
+                <input type="email" name="email" value={formData.email} onChange={handleChange} className='p-2 md:p-4 border block w-full rounded-md' placeholder="e.g. stephenking@lorem.com" />
             </div>
             <div className='space-y-2'>
                 <label htmlFor="number" className="md:flex justify-between">Phone Number
                     {error.number && <span className="text-red-500 font-bold block">{error.number}</span>}
                 </label>
-                <input type="text" name="number" value={formData.number} onChange={handleChange} className='p-2 md:p-4 border block w-full' placeholder="e.g. +1 234 567 890" />
+                <input type="text" name="number" value={formData.number} onChange={handleChange} className='p-2 md:p-4 border block w-full rounded-md' placeholder="e.g. +1 234 567 890" />
             </div>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,8 @@ export default {
     },
     extend: {
       backgroundImage: {
-        'sidebar-mobile': "url('./images/bg-sidebar-mobile.svg')",
-        'sidebar-desktop': "url('./images/bg-sidebar-desktop.svg')",
-        'active': '<svg class="w-4 h-4 m-auto text-white fill-current" viewBox="0 0 24 24"><path d="M9 16.17l-4.17-4.17L3 13l6 6 12-12-1.41-1.42L9 16.17z" /></svg>'
+        'sidebar-mobile': "url('../images/bg-sidebar-mobile.svg')",
+        'sidebar-desktop': "url('../images/bg-sidebar-desktop.svg')"
       }
     },
   },
